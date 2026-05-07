917bb4576dbbd47a12f6558f37e15c084a245503
kimjongsoo97 <rlwhdtn97@naver.com>
2026-05-07 18:30:18 +0900

    Feat: 백엔드 루트 이동 및 README 추가

---
+++ src/test/java/com/flow/backend/FlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.backend;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class FlowApplicationTests {
+
+    @Test
+    void applicationClassExists() {
+        assertThat(FlowApplication.class).isNotNull();
+    }
+}
